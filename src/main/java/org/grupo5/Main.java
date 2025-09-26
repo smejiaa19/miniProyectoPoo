603429750a2b48a7d51f29f4c93b7d87e190a9a9
Silvio Mejia <silviomejiagarcia@gmail.com>
2025-09-26 11:55:57 -0600

    probando algo

--- src/main/java/org/grupo5/Main.java
+++ src/main/java/org/grupo5/Main.java
@@ -19,6 +19,6 @@ public class Main {
             vehiculo.Recargar();
             System.out.println("---------------------------------------------------");
         }
-
+        System.out.println("Hola");
     }
 }
